1f9a712f98a14d6e030ffe11887dac732d2a3a81
cyhbyw <swust2009cyh@qq.com>
2018-06-24 16:54:52 +0800

    自己写的 MyBeanFactory extends DefaultListableBeanFactory 用来模拟 XmlBeanFactory

--- spring-context/src/test/java/com/cyh/haojia/chapter02/MyTestBeanTest.java
+++ spring-context/src/test/java/com/cyh/haojia/chapter02/MyTestBeanTest.java
@@ -1,16 +1,31 @@
 package com.cyh.haojia.chapter02;
 
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 public class MyTestBeanTest {
 
     public static void main(String[] args) {
-        XmlBeanFactory beanFactory =
-                new XmlBeanFactory(new ClassPathResource("com/cyh/haojia/chapter02/beanFactoryTest.xml"));
+        ClassPathResource resource = new ClassPathResource("com/cyh/haojia/chapter02/beanFactoryTest.xml");
+        MyBeanFactory beanFactory = new MyBeanFactory(resource);
         MyTestBean bean = (MyTestBean) beanFactory.getBean("myTestBean");
         System.out.println(bean.getTestStr());
     }
 
+
+    /**
+     * 自己写的BeanFactory
+     * 和 XmlBeanFactory 差不多，其内部实现也非常简单
+     */
+    static class MyBeanFactory extends DefaultListableBeanFactory {
+        private final XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(this);
+
+        public MyBeanFactory(Resource resource) {
+            super(null);
+            this.reader.loadBeanDefinitions(resource);
+        }
+    }
 
 }
