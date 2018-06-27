71d8c17cec539783c1c4d116475561c96bdd17e2
cyhbyw <swust2009cyh@qq.com>
2018-06-27 08:28:42 +0800

    给 obtainFreshBeanFactory() refreshBeanFactory() 方法添加的注释

--- spring-context/src/main/java/org/springframework/context/support/AbstractRefreshableApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/support/AbstractRefreshableApplicationContext.java
@@ -122,9 +122,12 @@ public abstract class AbstractRefreshableApplicationContext extends AbstractAppl
             closeBeanFactory();
         }
         try {
+            // ===============================创建 DefaultListableBeanFactory ======================================
             DefaultListableBeanFactory beanFactory = createBeanFactory();
             beanFactory.setSerializationId(getId());
+            // 定制BeanFactory：设置相关属性，包括是否允许覆盖同名称的不同定义的对象、循环依赖
             customizeBeanFactory(beanFactory);
+            // 初始化 DocumentReader，并进行XML文件读取及解析
             loadBeanDefinitions(beanFactory);
             synchronized (this.beanFactoryMonitor) {
                 this.beanFactory = beanFactory;
@@ -195,6 +198,7 @@ public abstract class AbstractRefreshableApplicationContext extends AbstractAppl
      * @see org.springframework.beans.factory.support.DefaultListableBeanFactory#setAllowRawInjectionDespiteWrapping
      */
     protected DefaultListableBeanFactory createBeanFactory() {
+        // ===============================创建 DefaultListableBeanFactory ======================================
         return new DefaultListableBeanFactory(getInternalParentBeanFactory());
     }
 
